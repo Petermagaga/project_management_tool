7cf8b6a898a1fa136112857d581eaeda2729e836
petermagaga <ptrmagaga@gmail.com>
2026-06-04 15:36:05 +0300

    core-settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -45,15 +45,37 @@ INSTALLED_APPS = [
     'notifications',
     'analytics_app',
 ]
+
 MIDDLEWARE = [
+
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
+
     'django.middleware.common.CommonMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+
     'django.contrib.messages.middleware.MessageMiddleware',
+
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+
+    ),
+
+    "DEFAULT_SCHEMA_CLASS":
+        "drf_spectacular.openapi.AutoSchema",
+}
 
 ROOT_URLCONF = 'core.urls'
 
