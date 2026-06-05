8a94c828e84dad1f8ad1ece2e0aed3c5574a2402
petermagaga <ptrmagaga@gmail.com>
2026-06-05 23:43:15 +0300

    static

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -155,7 +155,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
+MEDIA_URL = "/media/"
 
-STATIC_URL = 'static/'
+MEDIA_ROOT = BASE_DIR / "media"
+
+STATIC_URL = "/static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
