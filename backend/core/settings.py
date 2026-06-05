35af08f4e555a62563acbaf45fb984a6a2317c1d
petermagaga <ptrmagaga@gmail.com>
2026-06-06 00:04:38 +0300

    .env

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,16 +1,8 @@
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -103,8 +95,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE":"django.db.backends.postgresql",
+        "NAME":config('DB_NAME'),
+        "USER":config("DB_USER"),
+        "PASSWORD":config('DB_PASSWORD'),
+        "HOST":config('DB_HOST'),
+        "PORT":config('DB_PORT')
     }
 }
 
