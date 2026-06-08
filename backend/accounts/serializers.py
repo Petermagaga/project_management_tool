55640612e64b59d2c58a0881edf7a1a1269df8c8
petermagaga <ptrmagaga@gmail.com>
2026-06-08 16:10:01 +0300

    admin-accpount

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -6,4 +6,43 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model=User
-        
+
+        fields =[
+
+            "id",
+            "email",
+            "username",
+            "first_name",
+            "last_name",
+            "bio",
+            "job_title",
+            "avatar",
+            "created_at"
+        ]
+
+        read_only_fields=(
+            "id",
+            "created_at",
+        )
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(
+        write_only=True,
+        min_length=8
+    )       
+    class Meta:
+        model=User
+
+        fields=[
+
+            "email",
+            "username",
+            "password"
+        ]
+    def create(self,validated_data):
+        return User.objects.create_user(
+            email=validated_data["email"],
+            username=validated_data["username"],
+            password=validated_data["password"]
+        )
+    
