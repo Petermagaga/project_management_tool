8b9565cc035d00e3807f557392cca834cc3b6ced
petermagaga <ptrmagaga@gmail.com>
2026-06-09 23:25:12 +0300

    workspaces-mdels

--- backend/workspaces/models.py
+++ backend/workspaces/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Workspace(models.Model):
+    name=models.CharField(max_length=255)
+    description=models.TextField(blank=True)
+    owner=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,
+                            related_name="owned_workspaces")
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class WorkspaceMember(models.Model):
+    ROLES_choices=[
+        ("OWNER","Owner"),
+        ("ADMIN",'admin'),
+        ("MEMBER",'member'),
+        ("VIEWER","viewer"),
+    ]
+
+    workspace=models.ForeignKey(
+        Workspace,on_delete=models.CASCADE,
+        related_name="members"
+    )
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    role=models.CharField(choices=ROLES_choices,default="MEMBER")
+
+    joined_at=models.DateTimeField(
+        auto_now_add=True
+    )
+    class Meta:
+        unique_together=(
+            "workspace",
+            "user"
+        )
+
+    def __str__(self):
+        return f"{self.user.email} -{self.role}"
+
