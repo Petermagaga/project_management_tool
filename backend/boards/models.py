bc7506e6f5f3c4039d4c7def3e926141519cb447
petermagaga <ptrmagaga@gmail.com>
2026-06-11 11:56:54 +0300

    projects-views

--- backend/boards/models.py
+++ backend/boards/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from projects.models import Project
+
+class Board(models.Model):
+    project=models.ForeignKey(Project,on_delete=models.CASCADE,related_name="boards")
+    name=models.CharField(max_length=255)
+    position=models.PositiveBigIntegerField(default=0)
+
+    create_at=models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering=["position"]
+
+    def __str__(self):
+        return f"{self.project.name} - {self.name}"
