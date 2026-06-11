0eb6444969920115e052febc78b38a484c2cfa51
petermagaga <ptrmagaga@gmail.com>
2026-06-11 13:16:26 +0300

    signal.py

---
+++ backend/boards/serializers.py
@@ -0,0 +1,10 @@
+from .models import Board
+from rest_framework.serializers import ModelSerializer
+
+
+class BoardSerializer(ModelSerializer):
+    class Meta:
+        model=Board
+        fields="__all__"
+
+        
--- backend/boards/views.py
+++ backend/boards/views.py
@@ -1,3 +1,136 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import (
+    IsAuthenticated
+)
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from projects.models import Project
+from .models import Board
+from .serializers import BoardSerializer
+
+class CreateBoardView(generics.CreateAPIView):
+    serializer_class=BoardSerializer
+    permission_classes=[IsAuthenticated]
+
+
+class ProjectBoardsView(
+    APIView
+):
+
+    permission_classes = [
+        IsAuthenticated
+    ]
+
+    def get(
+        self,
+        request,
+        project_id
+    ):
+
+        boards = Board.objects.filter(
+            project_id=project_id
+        )
+
+        serializer = BoardSerializer(
+            boards,
+            many=True
+        )
+
+        return Response(
+            serializer.data
+        )
+
+
+class BoardDetailView(
+    APIView
+):
+
+    permission_classes = [
+        IsAuthenticated
+    ]
+
+    def get(
+        self,
+        request,
+        board_id
+    ):
+
+        board = get_object_or_404(
+            Board,
+            id=board_id
+        )
+
+        serializer = BoardSerializer(
+            board
+        )
+
+        return Response(
+            serializer.data
+        )
+    
+
+class UpdateBoardView(
+    APIView
+):
+
+    permission_classes = [
+        IsAuthenticated
+    ]
+
+    def put(
+        self,
+        request,
+        board_id
+    ):
+
+        board = get_object_or_404(
+            Board,
+            id=board_id
+        )
+
+        serializer = BoardSerializer(
+            board,
+            data=request.data,
+            partial=True
+        )
+
+        serializer.is_valid(
+            raise_exception=True
+        )
+
+        serializer.save()
+
+        return Response(
+            serializer.data
+        )
+    
+
+class DeleteBoardView(
+    APIView
+):
+
+    permission_classes = [
+        IsAuthenticated
+    ]
+
+    def delete(
+        self,
+        request,
+        board_id
+    ):
+
+        board = get_object_or_404(
+            Board,
+            id=board_id
+        )
+
+        board.delete()
+
+        return Response(
+            {
+                "message":
+                "Board deleted"
+            }
+        )
