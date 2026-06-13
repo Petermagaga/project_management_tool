49a76d336900a96f33bcc12842c14057a4236ea6
petermagaga <ptrmagaga@gmail.com>
2026-06-13 23:28:03 +0300

    models

--- backend/comments/admin.py
+++ backend/comments/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "task",
+        "user",
+        "created_at",
+    )
+
+    search_fields = (
+        "content",
+        "user__email",
+        "task__title",
+    )
