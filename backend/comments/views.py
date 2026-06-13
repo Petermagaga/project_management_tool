48421699e63dd3da6d82acccb09da8681099522f
petermagaga <ptrmagaga@gmail.com>
2026-06-13 23:33:26 +0300

    view-delete

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -34,3 +34,116 @@ class CreateCommentView(
         serializer.save(
             user=self.request.user
         )
+
+
+class TaskCommentsView(
+    APIView
+):
+
+    permission_classes = [
+        IsAuthenticated
+    ]
+
+    def get(
+        self,
+        request,
+        task_id
+    ):
+
+        comments = Comment.objects.filter(
+            task_id=task_id
+        ).order_by(
+            "created_at"
+        )
+
+        serializer = CommentSerializer(
+            comments,
+            many=True
+        )
+
+        return Response(
+            serializer.data
+        )
+    
+class UpdateCommentView(
+    APIView
+):
+
+    permission_classes = [
+        IsAuthenticated
+    ]
+
+    def put(
+        self,
+        request,
+        comment_id
+    ):
+
+        comment = get_object_or_404(
+            Comment,
+            id=comment_id
+        )
+
+        if comment.user != request.user:
+
+            return Response(
+                {
+                    "error":
+                    "You can only edit your own comments"
+                },
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        serializer = CommentSerializer(
+            comment,
+            data=request.data,
+            partial=True
+        )
+
+        serializer.is_valid(
+            raise_exception=True
+        )
+
+        serializer.save()
+
+        return Response(
+            serializer.data
+        )
+
+class DeleteCommentView(
+    APIView
+):
+
+    permission_classes = [
+        IsAuthenticated
+    ]
+
+    def delete(
+        self,
+        request,
+        comment_id
+    ):
+
+        comment = get_object_or_404(
+            Comment,
+            id=comment_id
+        )
+
+        if comment.user != request.user:
+
+            return Response(
+                {
+                    "error":
+                    "You can only delete your own comments"
+                },
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        comment.delete()
+
+        return Response(
+            {
+                "message":
+                "Comment deleted"
+            }
+        )
