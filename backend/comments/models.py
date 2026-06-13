b334ef74162fbd09521efe6e23ff8460b07f5c34
petermagaga <ptrmagaga@gmail.com>
2026-06-13 23:26:35 +0300

    models

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from tasks.models import Task
+
+
+class Comment(models.Model):
+
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return f"{self.user.email} - {self.task.title}"
