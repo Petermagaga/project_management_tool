5a7c0f96829a3823e2e4a5aefd2616266eea4ec2
petermagaga <ptrmagaga@gmail.com>
2026-06-14 20:57:25 +0300

    models-ADMIN

--- backend/notifications/admin.py
+++ backend/notifications/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ActivityLog
+
+
+@admin.register(ActivityLog)
+class ActivityLogAdmin(admin.ModelAdmin):
+
+    list_display = (
+
+        "id",
+
+        "project",
+
+        "user",
+
+        "action_type",
+
+        "created_at",
+    )
+
+    list_filter = (
+
+        "action_type",
+    )
