e27c61116eafd0d52b032303dff8da24fef064b6
petermagaga <ptrmagaga@gmail.com>
2026-06-15 21:14:30 +0300

    notification-services

---
+++ backend/notifications/services.py
@@ -0,0 +1,25 @@
+from .models import ActivityLog
+
+
+def create_activity_log(
+
+    project,
+
+    user,
+
+    action_type,
+
+    message
+
+):
+
+    ActivityLog.objects.create(
+
+        project=project,
+
+        user=user,
+
+        action_type=action_type,
+
+        message=message
+    )
